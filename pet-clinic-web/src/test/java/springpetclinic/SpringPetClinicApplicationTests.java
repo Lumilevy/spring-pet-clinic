c98b7a884b72d375292c7306ec4fa8ad50333b6a
Flora Pelletier <flora_pelletier@yahoo.com>
2018-11-20 11:43:39 +0100

    upgrade junit 5

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,11 +1,14 @@
 package springpetclinic;
 
 import guru.springframework.sfgpetclinic.SpringPetClinicApplication;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ExtendWith(SpringExtension.class)
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = SpringPetClinicApplication.class)
 public class SpringPetClinicApplicationTests {
